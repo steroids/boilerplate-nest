af1bd371ea1b76b28d0e8ba63cf8b02db44b846f
perlexed <perlexed@gmail.com>
2024-01-17 11:56:29 +0700

    CI/CD: log dir fixed

--- ecosystem.config.sample.js
+++ ecosystem.config.sample.js
@@ -4,6 +4,6 @@ module.exports = {
         script: 'dist/main.js',
         cwd: '/var/www/boilerplatenest12345',
         error_file: '/var/www/boilerplatenest12345/logs/nest-backend-error.log',
-        out_file: '/var/www/boilerplatenest12345/files/nest-backend-out.log',
+        out_file: '/var/www/boilerplatenest12345/logs/nest-backend-out.log',
     }],
 };
