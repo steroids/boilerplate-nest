d71433a29d185863c866d13a28933ff9e3e485bd
Danil <danil_ustinov@mail.ru>
2025-02-04 13:27:33 +0700

    fix: added APP_FILE_PREVIEW_ROTATE to env

--- src/file/infrastructure/FileModule.ts
+++ src/file/infrastructure/FileModule.ts
@@ -9,29 +9,6 @@ import {ModuleHelper} from '@steroidsjs/nest/infrastructure/helpers/ModuleHelper
         ...(coreModule.tables ?? []),
         ...ModuleHelper.importDir(__dirname + '/tables'),
     ],
-    config: () => {
-        const config = (coreModule?.config ?? (() => {}))();
-
-        return {
-            ...config,
-            previews: {
-                original: {
-                    ...(config?.previews?.original ?? {}),
-                    sharp: {
-                        ...(config?.previews?.original?.sharp ?? {}),
-                        rotate: true,
-                    },
-                },
-                thumbnail: {
-                    ...(config?.previews?.thumbnail ?? {}),
-                    sharp: {
-                        ...(config?.previews?.thumbnail?.sharp ?? {}),
-                        rotate: true,
-                    },
-                },
-            },
-        } as IFileModuleConfig;
-    },
     module: (config: IFileModuleConfig) => {
         if (!coreModule.module) {
             throw new Error('coreModule.module is not defined');
